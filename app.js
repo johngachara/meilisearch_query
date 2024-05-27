6196a8a7fd15a1cc62a31d47caf85ea663fbf7a4
John Gachara <johngachara29@gmail.com>
2024-05-27 06:18:07 +0300

    add port

--- app.js
+++ app.js
@@ -62,5 +62,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 module.exports = app;
